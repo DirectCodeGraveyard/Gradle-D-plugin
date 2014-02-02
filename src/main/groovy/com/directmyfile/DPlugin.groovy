698acb5973b0130fb259daf7eb034889e6e5bf2f
samrg472 <samrg472@gmail.com>
2014-02-01 19:01:31 -0500

    Add compileD to build task

--- src/main/groovy/com/directmyfile/DPlugin.groovy
+++ src/main/groovy/com/directmyfile/DPlugin.groovy
@@ -8,5 +8,8 @@ class DPlugin implements Plugin<Project> {
     void apply(Project target) {
         target.extensions.create('d', DExtension, target)
         target.task('compileD', type: CompileTask)
+
+        target.tasks.maybeCreate('build')
+        target.tasks.getByName('build').dependsOn('compileD')
     }
 }
